6c6378d1dc74f1a086112415395570eff5bd7bb3
Robin Lind <larsrobinlind@gmail.com>
2024-04-07 09:40:28 +0200

    Small fix to unproject

--- utils.hpp
+++ utils.hpp
@@ -23,6 +23,7 @@
 #ifndef UTILS_MATH_H
 #define UTILS_MATH_H
 
+#include "matrix.hpp"
 #include "vector.hpp"
 #include "quaternion.hpp"
 #include <cmath>
@@ -206,7 +207,7 @@ constexpr auto perpendicular(const vector<T, 3>& v)
 template<typename T>
 constexpr auto unproject(const vector<T, 3>& source, const matrix<T, 4, 4>& projection, const matrix<T, 4, 4>& view)
 {
-    const auto view_proj = mul(view, projection);
+    const auto view_proj = mul(transpose(view), projection); // why does view need to be transposed now?!
     const auto inv_view_proj = inverse(view_proj);
     const quaternion<T> quat(source.x, source.y, source.z, T(1));
     const auto qtransformed = transform(quat, inv_view_proj);
